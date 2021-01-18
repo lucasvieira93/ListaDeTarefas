af0c9f5838b58fe54734774784d58f7b7fa7b286
Lucas Vieira <lvieirastos@gmail.com>
2021-01-18 15:35:02 -0300

    Botão salvar no menu_main.xml implementado

--- app/src/main/java/com/example/listadetarefas/activity/AdicionarTarefaActivity.java
+++ app/src/main/java/com/example/listadetarefas/activity/AdicionarTarefaActivity.java
@@ -3,8 +3,13 @@ package com.example.listadetarefas.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.listadetarefas.R;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AdicionarTarefaActivity extends AppCompatActivity {
 
@@ -13,4 +18,28 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.item_salvar:
+                Toast.makeText(this, "Salvo!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void confirmarProcesso(View view){
+        Snackbar.make(view, "Funcionando!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+    }
+
 }
--- app/src/main/java/com/example/listadetarefas/activity/MainActivity.java
+++ app/src/main/java/com/example/listadetarefas/activity/MainActivity.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.listadetarefas.R;
-import com.example.listadetarefas.activity.AdicionarTarefaActivity;
 import com.example.listadetarefas.adapter.TarefaAdapter;
 import com.example.listadetarefas.helper.RecyclerItemClickListener;
 import com.example.listadetarefas.model.Tarefa;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -21,7 +21,6 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.LinearLayout;
 
@@ -110,25 +109,4 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
